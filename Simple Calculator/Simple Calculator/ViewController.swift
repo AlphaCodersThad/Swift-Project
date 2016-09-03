88e7297162f6c25f13ad2b9182bfd06b70ae965a
AlphaCodersThad <alphacodersthad@gmail.com>
2016-09-03 15:01:05 -0400

    outlet to some labels

--- Simple Calculator/Simple Calculator/ViewController.swift
+++ Simple Calculator/Simple Calculator/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var multiplicandLabel: UILabel!
+    
+    @IBOutlet weak var multiplierLabel: UILabel!
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
