45983734226add54c95729d977c3e94b01ee9386
AlphaCodersThad <alphacodersthad@gmail.com>
2016-09-26 02:12:29 -0400

    First commit

--- Pentaminoes Puzzle/Pentaminoes Puzzle/ViewController.swift
+++ Pentaminoes Puzzle/Pentaminoes Puzzle/ViewController.swift
@@ -10,16 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var largeBoard: UIImageView!
+    
+    let boardImage = UIImage(named: "Board0.png")
+    var boardImageView:UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        boardImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 420, height: 420))
+        boardImageView.contentMode = .ScaleAspectFill
+        boardImageView.image  = boardImage
+        view.addSubview(boardImageView)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
---
+++ Pentaminoes Puzzle/Pentaminoes Puzzle/puzzlePieces.swift
@@ -0,0 +1,9 @@
+//
+//  puzzlePieces.swift
+//  Pentaminoes Puzzle
+//
+//  Created by Thadea Achmad on 9/24/16.
+//  Copyright © 2016 Thadea Achmad. All rights reserved.
+//
+
+import Foundation
