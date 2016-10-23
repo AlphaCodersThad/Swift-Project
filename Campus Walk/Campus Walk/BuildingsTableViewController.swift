f193507e337998adda87d0326b2951487041a87f
AlphaCodersThad <alphacodersthad@gmail.com>
2016-10-22 23:27:14 -0400

    Setting up detail view controller

--- Campus Walk/Campus Walk/BuildingsTableViewController.swift
+++ Campus Walk/Campus Walk/BuildingsTableViewController.swift
@@ -10,7 +10,7 @@ import Foundation
 import UIKit
 import MapKit
 
-class BuildingsTableViewController: UITableViewController{
+class BuildingsTableViewController: UITableViewController, BuildingDetailDataSource {
     
     let buildingData = buildingModel.sharedInstance
     
@@ -41,27 +41,27 @@ class BuildingsTableViewController: UITableViewController{
         return cell
     }
     
-    /*var currentBuilding : Building {
+    var currentBuilding : buildingModel.Building {
         get {
-            return buildingData.buildingAtIndexPath(self.tableView.indexPathForSelectedRow!)
+            return buildingData.buildingAtPath(self.tableView.indexPathForSelectedRow!)
         }
-    }*/
+    }
     
     var indexPath : NSIndexPath {
         get { return tableView.indexPathForSelectedRow! }
     }
     
-    /*override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         switch segue.identifier! {
-        case "InfoSegue":
-            let infoViewController = segue.destinationViewController as! InfoViewController
-            let svc = self.tabBarController?.viewControllers![0].childViewControllers[0] as! CampusWalkViewController
-            infoViewController.dataSource = self
-            infoViewController.campusWalkDataSource = svc
-            infoViewController.completionBlock = { () in self.dismissViewControllerAnimated(true, completion: nil)}
-            break
+        case "DetailSegue":
+            if let detailViewController = segue.destinationViewController as? DetailViewController {
+                detailViewController.dataSource = self
+                //let annotation = mapView.selectedAnnotations.first! as! Model.Place
+                
+                //DetailViewController.configure(buildingData.buildingName, opp_bldg_code: buildingData.buildingOpp, year_constructed: buildingData.buildingYear, coordinate: buildingData.coordinate, isFavorite: buildingData.isFavorite, photoFile: buildingData.photoFile)
+            }
         default:
             assert(false, "Unhandled Segue")
         }
-    }*/
+    }
 }
---
+++ Campus Walk/Campus Walk/DetailViewController.swift
@@ -0,0 +1,72 @@
+//
+//  DetailViewController.swift
+//  Campus Walk
+//
+//  Created by Thadea Achmad on 10/22/16.
+//  Copyright © 2016 Thadea Achmad. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+
+protocol BuildingDetailDataSource: class {
+    var currentBuilding: buildingModel.Building { get }
+    var indexPath: NSIndexPath { get}
+}
+
+class DetailViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    weak var dataSource: BuildingDetailDataSource?
+    @IBOutlet weak var buildingImage: UIImageView!
+    @IBOutlet weak var buildingNameLabel: UILabel!
+    @IBOutlet weak var buildingYearLabel: UILabel!
+    @IBOutlet weak var favoriteButton: UIButton!
+    var _name: String = ""
+    var _opp_bldg_code: String = ""
+    var _year_constructed: String = ""
+    var _coordinate: String = ""
+    var _photoFile = ""
+    var _isFavorite: Bool?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //subtitleTextField.editable = false
+        _name = (dataSource?.currentBuilding.buildingName)!
+        _opp_bldg_code = String((dataSource?.currentBuilding.buildingOpp)!)
+        _year_constructed = String((dataSource?.currentBuilding.buildingYear)!)
+        _coordinate = " Latitude: \(dataSource?.currentBuilding.coordinate.latitude), Longitude: \(dataSource?.currentBuilding.coordinate.longitude)"
+        _photoFile = (dataSource?.currentBuilding.photoFile)!
+        _isFavorite = (dataSource?.currentBuilding.isFavorite)!
+        
+        
+        buildingNameLabel.text = _name
+        buildingYearLabel.text = _year_constructed
+        if !_isFavorite! {
+            favoriteButton.setTitle("Add to Favorite", forState: UIControlState.Normal)
+        }
+        buildingImage.image = UIImage(named: _photoFile)
+        
+        //subtitleTextField.flashScrollIndicators()
+        //subtitleTextField.contentOffset.y = 0
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*func configure(name: String, opp_bldg_code:String, year_constructed:String, coordinate:CLLocationCoordinate2D, isFavorite: Bool, photoFile: String){
+        _name = name
+        _opp_bldg_code = opp_bldg_code
+        _year_constructed = year_constructed
+        _coordinate = "Latitude: \(coordinate.latitude), Longitude: \(coordinate.longitude)"
+        _isFavorite = isFavorite
+        _photoFile = photoFile
+
+    }*/
+
+    
+}
