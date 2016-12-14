eebe1ce1ee2065ce346f5daf0908f1623a1be285
AlphaCodersThad <alphacodersthad@gmail.com>
2016-12-14 13:47:01 -0500

    First commit in a while... Hopefully framework remains intact

---
+++ App Playground/App Playground/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  App Playground
+//
+//  Created by Thadea Achmad on 11/15/16.
+//  Copyright © 2016 Thadea Achmad. All rights reserved.
+//
+
+import UIKit
+import EventKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Schedule Me/Pods/Target Support Files/Pods-Schedule Me/Pods-Schedule Me-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Schedule_Me : NSObject
+@end
+@implementation PodsDummy_Pods_Schedule_Me
+@end
